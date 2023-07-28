15a7f9d03cfe1195eb5a97c5aec8ffc09befc5ab
vanesa medina <vanesitamedina@gmail.com>
2023-07-27 21:53:20 -0500

    primera clase de programación imperativa

---
+++ main.js
@@ -0,0 +1,13 @@
+/* let elTitulo = document.getElementById("title")
+
+setTimeout(() => {
+    elTitulo.textContent = "un nuevo titulo"
+}, 3000); 
+
+*/
+
+let numeroUno = 15
+
+let numeroDos = 20
+
+console.log(numeroUno + numeroDos)
